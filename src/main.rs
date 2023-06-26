cf40414b751e9f729b33dcbdc0c3fcc89c933121
Thomas Timmer <thomastimmer11@hotmail.com>
2023-06-26 09:06:15 +0000

    feat: add json generator:

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use fake::{Dummy, Faker};
 use fake::faker::name::en::Name;
 use chrono::{DateTime, Utc};
 use fake::Fake;
+use serde::Serialize;
+use serde::Serializer;
+use serde::ser::SerializeSeq;
+use std::io::Write;
 
 #[derive(Debug, Dummy, serde::Serialize)]
 pub struct Order {
@@ -19,10 +23,11 @@ pub struct Order {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // println!("Hello, world!");
-    for amount in [10, 10000, 100000, 1000000] {
-        builder(b',', &format!("data/US-{amount}.csv"), amount)?;
-        builder(b';', &format!("data/EU-{amount}.csv"), amount)?;
-        builder(b'\t', &format!("data/EU-{amount}.tsv"), amount)?;
+    for amount in [1000, 10000, 100000] {
+        // csv_builder(b',', &format!("data/US-{amount}.csv"), amount)?;
+        // csv_builder(b';', &format!("data/EU-{amount}.csv"), amount)?;
+        // csv_builder(b'\t', &format!("data/EU-{amount}.tsv"), amount)?;
+        json_builder(&format!("data/{amount}.json"), amount)?;
     }
 
 
@@ -30,12 +35,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 
-fn builder(delimiter: u8, path: &str, amount: usize) -> Result<(), Box<dyn std::error::Error>> {
+fn csv_builder(delimiter: u8, path: &str, amount: usize) -> Result<(), Box<dyn std::error::Error>> {
     let mut writer = WriterBuilder::new().delimiter(delimiter).from_path(path).unwrap();
 
     for _ in 0..amount {
         writer.serialize(Faker.fake::<Order>())?;
     }
+
+    Ok(())
+}
+
+fn json_builder(path: &str, amount: usize) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::create(path)?;
+    let mut writer = serde_json::Serializer::new(file);
+
+    let mut seq = writer.serialize_seq(Some(amount))?;
+    for _ in 0..amount {
+        seq.serialize_element(&Faker.fake::<Order>())?;
+    }
+    seq.end()?;
 
     Ok(())
 }
